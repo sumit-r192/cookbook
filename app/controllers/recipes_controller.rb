66d01f3157a82079863a7e71fdbb6386e7d354b2
Sumit <sumit.raghuwanshi192@gmail.com>
2023-07-27 21:19:15 +0530

    Added the search logic for requested query

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 
 class RecipesController < ApplicationController
+  def index
+    cuisine = params[:cuisine]&.downcase
+    ingredient = params[:ingredient]&.downcase
+
+    @recipes = Recipe.includes(:ingredients)
+
+    # Search by cuisine
+    @recipes = @recipes.where('lower(cuisine) LIKE ?', "%#{cuisine}%") if cuisine.present?
+
+    # Search by ingredient
+    return unless ingredient.present?
+
+    @recipes = @recipes.joins(:ingredients)
+                       .where('lower(ingredients.name) LIKE ?', "%#{ingredient}%")
+                       .distinct
+  end
 end
